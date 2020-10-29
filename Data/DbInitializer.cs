bc2d6a90ea74461293912c622627a13c7e95041c
Herciu Norbert <herciunorbert@yahoo.com>
2020-10-29 20:37:35 +0200

    Statistics and Publishers

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -11,43 +11,109 @@ namespace Herciu_Norbert_Lab2.Data
         public static void Initialize(LibraryContext context)
         {
             context.Database.EnsureCreated();
+
             if (context.Books.Any())
             {
-                return; // BD a fost creata anterior
+                return;   // BD a fost creata anterior
             }
-            var books = new Book[]
+
+            var publishers = new Publisher[]
             {
-new Book{Title="Baltagul",Author="Mihail Sadoveanu",Price=Decimal.Parse("22")},
-new Book{Title="Enigma Otiliei",Author="George Calinescu",Price=Decimal.Parse("18")},
-new Book{Title="Maytrei",Author="Mircea Eliade",Price=Decimal.Parse("27")}
+
+            new Publisher{PublisherName="Humanitas",Adress="Str. Aviatorilor, nr. 40, Bucuresti"},
+            new Publisher{PublisherName="Nemira",Adress="Str. Plopilor, nr. 35, Ploiesti"},
+            new Publisher{PublisherName="Paralela 45",Adress="Str. Cascadelor, nr. 22, Cluj-Napoca"},
+
             };
-            foreach (Book s in books)
+            foreach (Publisher p in publishers)
             {
-                context.Books.Add(s);
+                context.Publishers.Add(p);
             }
             context.SaveChanges();
+
+            var books = new Book[]
+            {
+            new Book{Title="Baltagul",Author="Mihail Sadoveanu",Price=Decimal.Parse("22")},
+            new Book{Title="Enigma Otiliei",Author="George Calinescu",Price=Decimal.Parse("18")},
+            new Book{Title="Maytrei",Author="Mircea Eliade",Price=Decimal.Parse("27")},
+            new Book{Title="Panza de paianjen",Author="Cella Serghi",Price=Decimal.Parse("45")},
+            new Book{Title="Fata de hartie",Author="Guillame Musso",Price=Decimal.Parse("38")},
+            new Book{Title="De veghe in lanul de secara",Author="J.D. Salinger",Price=Decimal.Parse("28")},
+
+            };
+            foreach (Book b in books)
+            {
+                context.Books.Add(b);
+            }
+            context.SaveChanges();
+
+            var publishedbooks = new PublishedBook[]
+           {
+                new PublishedBook {
+                    BookID = books.Single(c => c.Title == "Maytrei" ).ID,
+                    PublisherID = publishers.Single(i => i.PublisherName == "Humanitas").ID
+                    },
+               new PublishedBook {
+                    BookID = books.Single(c => c.Title == "Enigma Otiliei" ).ID,
+                    PublisherID = publishers.Single(i => i.PublisherName == "Humanitas").ID
+                    },
+               new PublishedBook {
+                    BookID = books.Single(c => c.Title == "Baltagul" ).ID,
+                    PublisherID = publishers.Single(i => i.PublisherName == "Nemira").ID
+                    },
+               new PublishedBook {
+                    BookID = books.Single(c => c.Title == "Fata de hartie" ).ID,
+                    PublisherID = publishers.Single(i => i.PublisherName == "Paralela 45").ID
+                    },
+               new PublishedBook {
+                    BookID = books.Single(c => c.Title == "Panza de paianjen" ).ID,
+                    PublisherID = publishers.Single(i => i.PublisherName == "Paralela 45").ID
+                    },
+               new PublishedBook {
+                    BookID = books.Single(c => c.Title == "De veghe in lanul de secara" ).ID,
+                    PublisherID = publishers.Single(i => i.PublisherName == "Paralela 45").ID
+                    },
+           };
+
+            foreach (PublishedBook pb in publishedbooks)
+            {
+                context.PublishedBooks.Add(pb);
+            }
+            context.SaveChanges();
+
             var customers = new Customer[]
             {
-new Customer{CustomerID=1050,Name="Popescu Marcela",BirthDate=DateTime.Parse("1979-09-01")},
-new Customer{CustomerID=1045,Name="Mihailescu Cornel",BirthDate=DateTime.Parse("1969-07-08")},
+
+            new Customer{CustomerID=1050,Name="Popescu Marcela",BirthDate=DateTime.Parse("09-01-1979")},
+            new Customer{CustomerID=1045,Name="Mihailescu Cornel",BirthDate=DateTime.Parse("07-08-1979")},
+
             };
             foreach (Customer c in customers)
             {
                 context.Customers.Add(c);
             }
             context.SaveChanges();
+
             var orders = new Order[]
             {
-new Order{BookID=1,CustomerID=1050},
-new Order{BookID=3,CustomerID=1045},
-new Order{BookID=1,CustomerID=1045},
-new Order{BookID=2,CustomerID=1050},
+            new Order{BookID=1,CustomerID=1050,OrderDate=DateTime.Parse("25-02-2020")},
+            new Order{BookID=3,CustomerID=1045,OrderDate=DateTime.Parse("28-09-2020")},
+            new Order{BookID=1,CustomerID=1045,OrderDate=DateTime.Parse("28-10-2020")},
+            new Order{BookID=2,CustomerID=1050,OrderDate=DateTime.Parse("28-09-2020")},
+            new Order{BookID=4,CustomerID=1050,OrderDate=DateTime.Parse("28-09-2020")},
+            new Order{BookID=6,CustomerID=1050,OrderDate=DateTime.Parse("28-10-2020")},
             };
             foreach (Order e in orders)
             {
                 context.Orders.Add(e);
             }
             context.SaveChanges();
+
+            
+
+           
         }
+
     }
 }
+
--- Models/Book.cs
+++ Models/Book.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Herciu_Norbert_Lab2.Models
 {
@@ -10,7 +11,9 @@ namespace Herciu_Norbert_Lab2.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
         public ICollection<Order> Orders { get; set; }
+        public ICollection<PublishedBook> PublishedBooks { get; set; }
     }
 }
